6ef2357878f39535ceaaad3ecd16db73d63cdf52
Gabriel Bandinelli <bandinelligabriel1@gmail.com>
2024-06-06 13:38:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <?php
     require('php/php_connect.php');
 ?>  
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            eleve_sport
+        </title>
+    </head>
+    <body>
+        <?php 
+            echo '<a href = "php/eleve.php"> Nos Elèves </a><br>';
+            echo '<a href = "php/eleve_sport.php">Elèves sports</a><br>';
+        ?>
 
-<?php 
-    echo '<a href = "php/eleve.php"> Nos Elèves </a><br>';
-    echo '<a href = "php/ecole_sport.php"> Quels sports quelles écoles ? </a><br>';
-    echo '<a href = "php/eleve_sport.php">Elèves sports</a><br>';
-?>
+    </body>
+</html>  
 
