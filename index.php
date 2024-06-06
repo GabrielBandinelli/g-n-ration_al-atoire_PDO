d49c5050371e311b560a740bffe4655f01801d4e
Gabriel Bandinelli <bandinelligabriel1@gmail.com>
2024-06-06 13:32:17 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    require('php/php_connect.php');
+?>  
+
+<?php 
+    echo '<a href = "php/eleve.php"> Nos Elèves </a><br>';
+    echo '<a href = "php/ecole_sport.php"> Quels sports quelles écoles ? </a><br>';
+    echo '<a href = "php/eleve_sport.php">Elèves sports</a><br>';
+?>
+
